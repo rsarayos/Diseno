3eab55d1aa73b81dd09e5cfd8753e0c851fbd37e
rsarayos <raul.sauceda@potros.itson.edu.mx>
2024-04-20 00:07:21 -0700

    cambios para ejecutable
    se modificaron las versiones en el pom para poder generar el ejecutable del proyecto

---
+++ Citas_Medicas_Convertidores/src/main/java/org/itson/diseno/citas_medicas_convertidores/Citas_Medicas_Convertidores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.diseno.citas_medicas_convertidores;
+
+/**
+ *
+ * @author alex_
+ */
+public class Citas_Medicas_Convertidores {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
