ed4c26f14ade77d7f47bd05101a5c3bc0c1abfd6
ser3154 <159857682+ser3154@users.noreply.github.com>
2024-05-13 19:23:58 -0700

    Add files via upload
    ya esta con mi caso de uso

---
+++ Citas_Medicas_Presentacion/src/main/java/presentacion/RenderTabla.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package presentacion;
+
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+/**
+ *
+ * @author Serva
+ */
+public class RenderTabla extends DefaultTableCellRenderer {
+
+   
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        if (value instanceof JButton) {
+            JButton boton =(JButton)value;
+            return boton;
+        }
+        return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column); 
+    }
+    
+    
+}
